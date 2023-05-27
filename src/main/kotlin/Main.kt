6f5ec7fab4f1bf8a313e52cd99ea7ab92259bc88
Just161 <bo.niehaus@gmail.com>
2023-05-27 19:46:54 +0200

    changes

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -7,36 +7,57 @@ fun main() {
     var relevanteFrage = 0
     var evaluateAnotherQuestion = true
 
+    val text1 = "Dieses Tool hilft bei der Analyse folgender Frage:\n"
+    val text2 = "\n\u001B[33mIst es pädagogisch sinnvoller, Fragen bis zum Ende einer Unterrichtseinheit zurückzuhalten und dann den Schüler*innen die Wahl zu lassen,\nob sie bleiben und die Fragen der anderen Kursteilnehmenden anhören möchten - oder ist ein prozessorientierter Ansatz,\nbei dem individuelle Fragen während des Unterrichts behandelt werden, effektiver?\n"
+    val text3 = "\n\u001B[0mDie Funktionen des Programms basieren auf der Erhebung und Auswertung von zwei simplen Fragen:\n1. \"Hätte die gerade gestellte Frage des Teilnehmenden eigenständig über Google beantwortet werden können?\" und\n2. \"War die Frage für den gesamten Kurs relevant?\"\n"
+    val text4 = "\nNatürlich ergibt sich daraus die Voraussetzung, dass im zu untersuchenden Kurs der prozessorientierte Ansatz verfolgt wird.\nDann kann dieses Tool während des Unterrichts genutzt werden, um bei jeder von einem Kursteilnehmenden gestellten Frage zu erörtern,\nob diese Frage relevant für den gesamten Kurs war, oder ob sie nur Zeit geraubt und Verwirrung gestiftet hat."
+    for (i in 1..25) {
+        println("")
+    }
+    println(text1 + text2 + text3 + text4)
+    println("")
+    println("Bereit für dein wissenschaftliches Abenteuer?\nDann drücke 'Enter'!")
+    readln()
+    for (i in 1..10) {
+        println("")
+    }
+    println("Das Programm wiederholt die folgenden zwei Fragen, speichert die Antworten und gibt,\nwenn dem Programm der Abschluss signalisiert wird, die Analyse der Antworten.")
+    println("")
     while (evaluateAnotherQuestion) {
-        println("Hast du die Frage schon gegoogelt? (ja/nein)")
+        println("# # # # # # # # # # # # # # # # # # # # # # # # # # # # # #")
+        println("\u001B[33mFrage 1: Wäre die Frage des Kursteilnehmenden von ihm*ihr googlebar gewesen? (ja/nein)")
         val googlebar = scanner.next().lowercase() == "ja"
-
-        println("Ist die Frage wichtig für den gesamten Kurs? (ja/nein)")
+        println("Frage 2: Diente die Frage nur dem eigenen Interesse und hatte sie keinen Mehrwert für die gesamte Klasse? (ja/nein)")
         val wichtig4Kurs = scanner.next().lowercase() == "ja"
 
         val trueCount = listOf(googlebar, wichtig4Kurs).count { it }
 
         if (trueCount >= 2) {
-            relevanteFrage++
-        } else {
             irrelevanteFrage++
+        } else {
+            relevanteFrage++
         }
 
         totalQuestions++
-
-        println("Möchten Sie eine weitere Frage bewerten? (ja/nein)")
+        println("\u001B[0mMöchten Sie eine weitere Frage bewerten? (ja/nein)")
         val userInput = scanner.next()
         evaluateAnotherQuestion = userInput.lowercase() == "ja"
-
+        for (i in 1..10) {
+            println("")
+        }
         if (!evaluateAnotherQuestion) {
-            println("# # # # # # # # # # # # # # # # # # #")
-            println("Auswertung:")
-            println("Gestellte Fragen: $totalQuestions")
-            println("Irrelevante Fragen: $irrelevanteFrage")
-            println("Relevante Fragen: $relevanteFrage")
-            println("Aus der Erhebung ergibt sich, dass ${irrelevanteFrage.toDouble() / totalQuestions.toDouble() * 100}% der gestellten Fragen irrelevant für die meisten Kursteilnehmer sind.")
-            println("Das Programm empfiehlt, dass Fragen erst nach der Vorlesung gestellt werden, sodass andere Teilnehmer nicht abgelenkt oder aufgehalten werden.")
-            println("# # # # # # # # # # # # # # # # # # #")
+            println("\u001B[33m# # # # # # # # # # # # # # # # # # # # # # # # # # # # # #\u001B[0m")
+            println("")
+            println("\u001B[33mAuswertung:\u001B[0m")
+            println("")
+            println("\u001B[33mGestellte Fragen: $totalQuestions\u001B[0m")
+            println("\u001B[33mIrrelevante Fragen: $irrelevanteFrage\u001B[0m")
+            println("\u001B[33mRelevante Fragen: $relevanteFrage\u001B[0m")
+            println("")
+            println("\u001B[33mAus der Erhebung ergibt sich, dass ${irrelevanteFrage.toDouble() / totalQuestions.toDouble() * 100}% der gestellten Fragen irrelevant für die meisten Kursteilnehmenden sind.\u001B[0m")
+            println("\u001B[33mDas Programm empfiehlt, dass Fragen erst nach der Vorlesung gestellt werden, sodass andere Teilnehmende nicht abgelenkt oder aufgehalten werden.\u001B[0m")
+            println("")
+            println("\u001B[33m# # # # # # # # # # # # # # # # # # #\u001B[0m")
         }
     }
 }
